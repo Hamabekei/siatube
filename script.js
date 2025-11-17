f313101227adf5ff276f1fce3cd6b94cb7f7b520
ほうっjk <r1807105@fkc.ritsumei.ac.jp>
2025-11-17 15:07:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_API_KEY_HERE";
+const API_KEY = "AIzaSyApWogGRphQ9_GnG8dX_JLXdAn2BtONx_Y";
 
 async function search() {
   const query = document.getElementById("query").value;
